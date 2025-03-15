16225cb86299655460e284bd36c7c0dcbab406da
Timothy Gummer <timothy.gummer@finity.com.au>
2025-03-16 00:15:14 +1100

    Many changes

---
+++ R/b1_data_exports/af_team_ids.R
@@ -0,0 +1,20 @@
+box::use(
+  ../a1_modules/af_pipelines
+  )
+box::use(
+  dotenv[load_dot_env],
+  here[here],
+  dplyr[...],
+  arrow[write_parquet]
+)
+
+load_dot_env() # a .env file doesn't exist, create one with your session ID in the R folder
+session_id <- Sys.getenv("AF_SESSION_ID")
+
+af_team_ids <- 1:100 |>
+  af_pipelines$team_user_ids(session_id = session_id)
+
+write_parquet(af_team_ids, here("../data/exports/2025", "af_team_ids.parquet"))
+
+
+
--- R/zz_adhoc/test_box.R
+++
@@ -1,5 +0,0 @@
-box::use(
-  ../a1_modules/af_hit_api[gp = get_players]
-)
-
-x <- gp()
