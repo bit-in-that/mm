3f9e24815555724262946a28cf7db01ab04fa104
Timothy Gummer <timothy.gummer@finity.com.au>
2025-03-16 10:27:04 +1100

    Fix action

--- R/b1_data_exports/af_team_ids.R
+++ R/b1_data_exports/af_team_ids.R
@@ -11,7 +11,11 @@ box::use(
 load_dot_env() # a .env file doesn't exist, create one with your session ID in the R folder
 session_id <- Sys.getenv("AF_SESSION_ID")
 
+print(session_id)
+
 af_team_ids <- 1:100 |>
   af_pipelines$team_user_ids(session_id = session_id)
+
+print(af_team_ids)
 
 write_parquet(af_team_ids, here("data/exports/2025", "af_team_ids.parquet"))
