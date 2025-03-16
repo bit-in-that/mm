4cf78517497fee005f83b65dec56eb3e61bfa28c
Timothy Gummer <timothy.gummer@finity.com.au>
2025-03-16 23:16:17 +1100

    Add exports for mm

--- R/b1_data_exports/af_team_ranks.R
+++ R/b1_data_exports/af_team_ranks.R
@@ -6,10 +6,19 @@ box::use(
   dotenv[load_dot_env],
   here[here],
   dplyr[...],
-  arrow[write_parquet]
+  purrr[...],
+  arrow[write_parquet, read_parquet]
 )
+
+resp_body_rounds <- af_api$get_rounds()
+
+
+
 
 load_dot_env() # a .env file doesn't exist, create one with your session ID in the R folder
 session_id <- Sys.getenv("AF_SESSION_ID")
 
+af_team_ids <- arrow::read_parquet(here("data/exports/2025", "af_team_ids.parquet"))
+
 # TODO: complete this code using the user_ids dataset and the other code
+
