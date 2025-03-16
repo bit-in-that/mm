0a1391f91211ced986083ffcf815f0bc1764edc3
Timothy Gummer <timothy.gummer@finity.com.au>
2025-03-16 11:16:56 +1100

    Create an action that will do all the team ids

--- R/b1_data_exports/af_team_ids.R
+++ R/b1_data_exports/af_team_ids.R
@@ -12,7 +12,13 @@ box::use(
 load_dot_env() # a .env file doesn't exist, create one with your session ID in the R folder
 session_id <- Sys.getenv("AF_SESSION_ID")
 
-af_team_ids <- 1:100 |>
+team_count <- af_pipelines$team_count(session_id)
+
+af_team_ids <- team_count |>
+  seq() |>
   af_pipelines$team_user_ids(session_id = session_id)
 
-write_parquet(af_team_ids, here("data/exports/2025", "af_team_ids.parquet"))
+if(!is.null(af_team_ids)) {
+  write_parquet(af_team_ids, here("data/exports/2025", "af_team_ids.parquet"))
+
+}
